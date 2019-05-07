1ee14c53d7a1937bb9701deec0d76cd2c532e9a1
diana pardy <dianapardy91@gmail.com>
2019-05-07 12:16:30 -0600

    Read from resource folder

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,15 +15,17 @@ public class CsvUtils {
 
     public static String readFile(String path) {
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            InputStream inputStream = new CsvUtils().getClass()
+                    .getClassLoader().getResourceAsStream(path);
+
+            Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
             } else {
                 return "";
             }
-
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
